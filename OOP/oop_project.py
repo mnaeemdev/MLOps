a7a8591ff6fd8a54381fceca9ddbc691a72659da
mnaeemdev <mnaeemraj.dev@gmail.com>
2025-12-24 14:33:11 +0500

    Sign In and Sign Up features added

--- OOP/oop_project.py
+++ OOP/oop_project.py
@@ -5,22 +5,20 @@ class chatbook:
         self.logged_in = False
         self.menu()
 
-    def menu(self):
+    def menu(self):  # Main menu method
         while True:
             print("Welcome to ChatBook!")
             print("1. Sign Up")
-            print("2. Log In")
+            print("2. Log/Sign In")
             print("3. Message to friends")
             print("4. Write a post")
             print("Press any other key to exit")
             choice = input("Choose an option: ")
 
             if choice == '1':
-                pass
-                # self.sign_up()
+                self.sign_up()
             elif choice == '2':
-                pass
-                # self.log_in()   
+                self.sign_in() 
             elif choice == '3':
                 pass
                 # self.message_friends()
@@ -31,5 +29,30 @@ class chatbook:
                 print("Exiting ChatBook. Goodbye!")
                 break
 
-    
-obj = chatbook() 
+
+    def sign_up(self):
+        print("Sign Up")
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        self.username = username
+        self.password = password
+        print("Signed up successful!")
+        print("\n")
+        self.menu()
+
+    def sign_in(self):
+        print("Log In")
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        if username == self.username and password == self.password:
+            self.logged_in = True
+            print("Logged in successfully!")
+        else:
+            print("Invalid credentials. Please try again. / Not Signed up first. \nPlease Sign Up First.")
+        print("\n")
+        self.menu()
+
+
+
+# Creating an instance of the chatbook class to run the program
+obj = chatbook()
